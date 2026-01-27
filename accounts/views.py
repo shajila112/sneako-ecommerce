0ac35f1214c94bbcb01405c9edade304fd1d31d1
shajila <shajilamuhammadali@gmail.com>
2026-01-27 14:45:45 +0530

    pushed

--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,17 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import 
+from .forms import RegisterForm
+# ,AuthenticationForm
+from django.contrib.auth import login
+# logout
+
+def register_view(request):
+    if request.method=="POST":
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request,user)
+            
+            return redirect('home')
+    else:
+        form=RegisterForm()
+    return render(request,'accounts/register.html',{'form':form})
--- sneako_project/urls.py
+++ sneako_project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls,),
    
     path('', include('store.urls')),
+    path('accounts',include('accounts.urls')),
     # path('cart/',include('cart.urls')),
     
 ]
