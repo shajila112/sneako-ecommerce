5428c188fe1f1d263ad4284704b66e0f81f7cef7
shajila <shajilamuhammadali@gmail.com>
2026-03-17 15:46:45 +0530

    fix email config

--- sneako_project/settings.py
+++ sneako_project/settings.py
@@ -165,8 +165,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'shajilamuhammadali@gmail.com'
+EMAIL_HOST_PASSWORD = 'igvc anyt aspj znmf'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
